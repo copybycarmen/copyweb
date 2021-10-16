a9c33d84fbec890774847052bbee678c126529d6
Aniceto <anibuendia3@gmail.com>
2021-10-16 13:32:06 +0200

    Button bug solved

--- public/app.js
+++ public/app.js
@@ -2,6 +2,5 @@ const btn = document.querySelector ('.menu-button')
 const sidebar = document.querySelector ('.sidebar')
 
 btn.addEventListener('click', () => {
-    sidebar.classList.toggle("translate-y-full");
-
+    sidebar.classList.toggle("hidden");
 })
