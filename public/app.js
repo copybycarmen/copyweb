967c6b3771c5aaeffb115c9ee83a8f5eb2ab8cfd
Aniceto <anibuendia3@gmail.com>
2021-10-16 14:45:40 +0200

    Change sidebar open click

--- public/app.js
+++ public/app.js
@@ -2,5 +2,6 @@ const btn = document.querySelector ('.menu-button')
 const sidebar = document.querySelector ('.sidebar')
 
 btn.addEventListener('click', () => {
-    sidebar.classList.toggle("-translate-x-full");
+    sidebar.classList.toggle("translate-y-full");
+    sidebar.classList.toggle("hidden");
 })
